f730778767993ea2ed02a01b959a0dd030e1594f
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-09-06 23:42:42 -0500

    Renamed folder

--- BluetoothExplorerIOS/Carthage/Checkouts/Bluetooth/Package.swift
+++
@@ -1,9 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Bluetooth",
-    targets: [
-        Target(
-            name: "Bluetooth")
-    ]
-)
--- BluetoothExplorerIOS/Carthage/Checkouts/Bluetooth/Sources/LowEnergyChannelMap.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LowEnergyChannelMap.swift
-//  Bluetooth
-//
-//  Created by Marco Estrella on 3/29/18.
-//  Copyright © 2018 PureSwift. All rights reserved.
-//
-
-public typealias LowEnergyChannelMap = (UInt8, UInt8, UInt8, UInt8, UInt8)

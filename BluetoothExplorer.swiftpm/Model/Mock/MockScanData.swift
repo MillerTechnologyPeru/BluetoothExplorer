8439af57bd104510ef6a5abaa8723afa6ad8ab2e
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-12-18 01:19:55 -0500

    Added Swift Playgrounds app

---
+++ BluetoothExplorer.swiftpm/Model/Mock/MockScanData.swift
@@ -0,0 +1,24 @@
+//
+//  MockScanData.swift
+//  BluetoothExplorer
+//
+//  Created by Alsey Coleman Miller on 31/10/21.
+//  Copyright © 2021 Alsey Coleman Miller. All rights reserved.
+//
+
+import Foundation
+import Bluetooth
+import GATT
+
+typealias MockScanData = ScanData<GATT.Peripheral, MockAdvertisementData>
+
+extension MockScanData {
+    
+    static let beacon = MockScanData(
+        peripheral: Peripheral(id: BluetoothAddress(rawValue: "00:1A:7D:DA:71:13")!),
+        date: Date(timeIntervalSinceReferenceDate: 10_000),
+        rssi: -20,
+        advertisementData: .beacon,
+        isConnectable: true
+    )
+}
---
+++ BluetoothExplorer.swiftpm/Model/NativeCentral.swift
@@ -0,0 +1,30 @@
+//
+//  Central.swift
+//  BluetoothExplorer
+//
+//  Created by Alsey Coleman Miller on 6/9/19.
+//  Copyright © 2019 Alsey Coleman Miller. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import Bluetooth
+import GATT
+import DarwinGATT
+
+typealias NativeCentral = DarwinCentral
+typealias NativePeripheral = DarwinCentral.Peripheral
+typealias NativeScanData = ScanData<NativePeripheral, DarwinAdvertisementData>
+
+extension NativeCentral {
+    
+    private struct Cache {
+        static let central = NativeCentral(
+            options: .init(showPowerAlert: true)
+        )
+    }
+    
+    static var shared: NativeCentral {
+        return Cache.central
+    }
+}

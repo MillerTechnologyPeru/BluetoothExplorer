17dc75a101bcc8d02d659637dba3785c0fef28fb
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-12-23 19:39:52 -0500

    Added `Array.init(hexadecimal:)`

---
+++ BluetoothExplorer.swiftpm/Extensions/Hexadecimal.swift
@@ -0,0 +1,60 @@
+//
+//  Hexadecimal.swift
+//  Bluetooth
+//
+//  Created by Alsey Coleman Miller on 3/2/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+internal extension FixedWidthInteger {
+    
+    func toHexadecimal() -> String {
+        
+        var string = String(self, radix: 16)
+        while string.utf8.count < (MemoryLayout<Self>.size * 2) {
+            string = "0" + string
+        }
+        assert(string.utf8.count == MemoryLayout<Self>.size * 2)
+        return string.uppercased()
+    }
+    
+    init?<T: StringProtocol>(hexadecimal string: T) {
+        self.init(string, radix: 16)
+    }
+}
+
+internal extension Collection where Element: FixedWidthInteger {
+    
+    func toHexadecimal() -> String {
+        let length = count * MemoryLayout<Element>.size * 2
+        var string = ""
+        string.reserveCapacity(length)
+        string = reduce(into: string) { $0 += $1.toHexadecimal() }
+        assert(string.count == length)
+        return string
+    }
+}
+
+internal extension Array where Element: FixedWidthInteger {
+    
+    init?(hexadecimal string: String) {
+        let elementStringSize = MemoryLayout<Self>.size * 2 // 2 for UInt8
+        guard string.count % elementStringSize == 0 else {
+            return nil
+        }
+        let elementsCount = string.count / elementStringSize
+        let elements = (0 ..< elementsCount)
+            .lazy
+            .map { string.index(string.startIndex, offsetBy: $0) ..< string.index(string.startIndex, offsetBy: $0 + elementStringSize) }
+            .map { string[$0] }
+        self.init()
+        self.reserveCapacity(elementsCount)
+        for substring in elements {
+            guard let element = Element.init(hexadecimal: substring) else {
+                return nil
+            }
+            self.append(element)
+        }
+        assert(self.count == elementsCount)
+    }
+}

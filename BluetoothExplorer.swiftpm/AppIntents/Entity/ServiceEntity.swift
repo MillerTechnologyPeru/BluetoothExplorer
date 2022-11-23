e16f7aba09da9d6e22625e1a364beb8560b592a8
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-11-23 11:15:21 -0800

    Add `DiscoverServicesIntent`

---
+++ BluetoothExplorer.swiftpm/AppIntents/Entity/ServiceEntity.swift
@@ -0,0 +1,87 @@
+//
+//  ServiceEntity.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 11/22/22.
+//
+
+import AppIntents
+import Bluetooth
+import GATT
+
+@available(macOS 13, iOS 16, watchOS 9, tvOS 16, *)
+struct ServiceEntity: AppEntity, Identifiable {
+    
+    let id: ID
+    
+    let uuid: String
+    
+    let isPrimary: Bool
+}
+
+@available(macOS 13, iOS 16, watchOS 9, tvOS 16, *)
+extension ServiceEntity {
+    
+    struct ID: Equatable, Hashable, EntityIdentifierConvertible, Sendable {
+        
+        let peripheral: UUID
+        
+        let attributeID: Int
+        
+        var entityIdentifierString: String {
+            return peripheral.uuidString + "/" + attributeID.description
+        }
+
+        /// Identifiers should be able to initialize via a `String` format.
+        static func entityIdentifier(for string: String) -> ID? {
+            let components = string.components(separatedBy: "/")
+            guard components.count == 2, let peripheral = UUID(uuidString: components[0]), let attributeID = Int(components[1]) else {
+                return nil
+            }
+            return ServiceEntity.ID.init(peripheral: peripheral, attributeID: attributeID)
+        }
+    }
+}
+
+@available(macOS 13, iOS 16, watchOS 9, tvOS 16, *)
+extension ServiceEntity {
+    
+    init(_ service: Store.Service) {
+        self.id = .init(peripheral: service.peripheral.id, attributeID: service.id.hashValue)
+        self.uuid = service.uuid.rawValue
+        self.isPrimary = service.isPrimary
+    }
+}
+
+@available(macOS 13, iOS 16, watchOS 9, tvOS 16, *)
+extension ServiceEntity {
+    
+    static var defaultQuery: ServiceQuery { ServiceQuery() }
+    
+    static var typeDisplayRepresentation: TypeDisplayRepresentation {
+        "Service"
+    }
+    
+    var displayRepresentation: DisplayRepresentation {
+        DisplayRepresentation(
+            title: "\(BluetoothUUID(rawValue: uuid)?.name ?? uuid)",
+            subtitle: "Peripheral \(id.peripheral.description)"
+        )
+    }
+}
+
+@available(macOS 13, iOS 16, watchOS 9, tvOS 16, *)
+struct ServiceQuery: EntityQuery {
+    
+    @MainActor
+    func entities(for identifiers: [ServiceEntity.ID]) -> [ServiceEntity] {
+        let allServices = Store.shared.services.values.lazy.reduce([], { $0 + $1 })
+        return identifiers.compactMap { id in
+            allServices
+                .first(where: { $0.peripheral.id == id.peripheral && $0.id.hashValue == id.attributeID })
+                .map { ServiceEntity($0) }
+        }
+    }
+    
+    func suggestedEntities() -> [ServiceEntity] { [] }
+}
---
+++ BluetoothExplorer.swiftpm/AppIntents/Intent/DiscoverServicesIntent.swift
@@ -0,0 +1,47 @@
+//
+//  DiscoverServicesIntent.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 11/22/22.
+//
+
+import AppIntents
+import SwiftUI
+import Bluetooth
+import GATT
+
+@available(macOS 13, iOS 16, watchOS 9, tvOS 16, *)
+struct DiscoverServicesIntent: AppIntent {
+    
+    static var title: LocalizedStringResource { "Discover services for a specified device." }
+    
+    static var description: IntentDescription {
+        IntentDescription(
+            "Discover services for a specified device.",
+            categoryName: "Utility",
+            searchKeywords: ["discovery", "services", "bluetooth"]
+        )
+    }
+    
+    @Parameter(
+        title: "The Bluetooth device to connect to.",
+        description: "The device to connect to."
+    )
+    var device: PeripheralEntity
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        let store = Store.shared
+        guard let peripheral = store.scanResults.keys.first(where: { $0.id == device.id }) else {
+            throw CentralError.unknownPeripheral
+        }
+        try await store.central.wait(for: .poweredOn, warning: 1, timeout: 2)
+        try await store.discoverServices(for: peripheral)
+        let services = store.services[peripheral, default: []].map {
+            ServiceEntity($0)
+        }
+        return .result(
+            value: services
+        )
+    }
+}

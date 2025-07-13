edef2c4008a789a016b8cd0bb55cbf4469533a8f
Alsey Coleman Miller <alseycmiller@gmail.com>
2025-07-13 03:39:16 -0400

    Copy files

---
+++ bluetooth-explorer-android/Sources/BluetoothExplorerModel/Model/Mock/MockAdvertisement.swift
@@ -0,0 +1,74 @@
+//
+//  MockAdvertisement.swift
+//  BluetoothExplorer
+//
+//  Created by Alsey Coleman Miller on 31/10/21.
+//  Copyright © 2021 Alsey Coleman Miller. All rights reserved.
+//
+
+#if DEBUG
+import Foundation
+import Bluetooth
+import GATT
+
+/// Mock Advertisement Data
+internal struct MockAdvertisementData: AdvertisementData {
+    
+    /// The local name of a peripheral.
+    internal let localName: String?
+    
+    /// The Manufacturer data of a peripheral.
+    internal let manufacturerData: ManufacturerSpecificData<Data>?
+    
+    /// This value is available if the broadcaster (peripheral) provides its Tx power level in its advertising packet.
+    /// Using the RSSI value and the Tx power level, it is possible to calculate path loss.
+    internal let txPowerLevel: Double?
+    
+    /// Service-specific advertisement data.
+    internal let serviceData: [BluetoothUUID: Data]?
+    
+    /// An array of service UUIDs
+    internal let serviceUUIDs: [BluetoothUUID]?
+    
+    /// An array of one or more `BluetoothUUID`, representing Service UUIDs.
+    internal let solicitedServiceUUIDs: [BluetoothUUID]?
+    
+    internal init(
+        localName: String? = nil,
+        manufacturerData: ManufacturerSpecificData<Data>? = nil,
+        txPowerLevel: Double? = nil,
+        serviceData: [BluetoothUUID : Data]? = nil,
+        serviceUUIDs: [BluetoothUUID]? = nil,
+        solicitedServiceUUIDs: [BluetoothUUID]? = nil
+    ) {
+        self.localName = localName
+        self.manufacturerData = manufacturerData
+        self.txPowerLevel = txPowerLevel
+        self.serviceData = serviceData
+        self.serviceUUIDs = serviceUUIDs
+        self.solicitedServiceUUIDs = solicitedServiceUUIDs
+    }
+}
+
+extension MockAdvertisementData {
+    
+    internal static let beacon = MockAdvertisementData(
+        localName: nil,
+        manufacturerData: ManufacturerSpecificData(data: Data([0x4c, 0x00, 0x02, 0x15, 0xb9, 0x40, 0x7f, 0x30, 0xf5, 0xf8, 0x46, 0x6e, 0xaf, 0xf9, 0x25, 0x55, 0x6b, 0x57, 0xfe, 0x6d, 0x29, 0x4c, 0x90, 0x39, 0x74])),
+        txPowerLevel: nil,
+        serviceData: nil,
+        serviceUUIDs: nil,
+        solicitedServiceUUIDs: nil
+    )
+    
+    internal static let smartThermostat = MockAdvertisementData(
+        localName: "CLI-W200",
+        manufacturerData: ManufacturerSpecificData(data: Data([0xd9, 0x01, 0x01, 0x02, 0x00, 0x00, 0x8c, 0x85, 0x90, 0xcb, 0x31, 0x74, 0x00, 0x60])),
+        txPowerLevel: nil,
+        serviceData: nil,
+        serviceUUIDs: [BluetoothUUID.Member.savantSystems2],
+        solicitedServiceUUIDs: nil
+    )
+}
+
+#endif
---
+++ bluetooth-explorer-android/Sources/BluetoothExplorerModel/Model/Store.swift
@@ -0,0 +1,363 @@
+//
+//  Store.swift
+//  BluetoothExplorer
+//
+//  Created by Alsey Coleman Miller on 6/9/19.
+//  Copyright © 2019 Alsey Coleman Miller. All rights reserved.
+//
+
+import Foundation
+import Observation
+import Bluetooth
+import GATT
+
+/// Store
+@MainActor
+@Observable
+public final class Store: @unchecked Sendable {
+    
+    typealias Central = NativeCentral
+    
+    typealias Peripheral = Central.Peripheral
+    
+    typealias ScanData = GATT.ScanData<Central.Peripheral, Central.Advertisement>
+    
+    typealias Service = GATT.Service<Central.Peripheral, Central.AttributeID>
+    
+    typealias Characteristic = GATT.Characteristic<Central.Peripheral, Central.AttributeID>
+    
+    typealias Descriptor = GATT.Descriptor<Central.Peripheral, Central.AttributeID>
+    
+    typealias ScanResult = ScanDataCache<Central.Peripheral, Central.Advertisement>
+    
+    // MARK: - Properties
+    
+    private(set) var activity = [Peripheral: Bool]()
+    
+    private(set) var isEnabled = false
+    
+    private(set) var scanResults = [Peripheral: ScanResult]()
+    
+    var isScanning: Bool {
+        self.scanStream?.isScanning ?? false
+    }
+    
+    private(set) var connected: Set<Peripheral> = []
+    
+    private(set) var services = [Peripheral: [Service]]()
+    
+    private(set) var characteristics = [Service: [Characteristic]]()
+    
+    private(set) var includedServices = [Service: [Service]]()
+
+    private(set) var descriptors = [Characteristic: [Descriptor]]()
+
+    private(set) var characteristicValues = [Characteristic: Cache<AttributeValue>]()
+    
+    private(set) var descriptorValues = [Descriptor: Cache<AttributeValue>]()
+
+    private(set) var isNotifying = [Characteristic: Bool]()
+    
+    internal let central: Central
+    
+    private var scanStream: AsyncCentralScan<NativeCentral>?
+        
+    // MARK: - Initialization
+    
+    public convenience init() {
+        let central = Central()
+        self.init(central: central)
+    }
+    
+    init(central: Central) {
+        self.central = central
+        setupLog()
+        observeValues()
+    }
+        
+    // MARK: - Methods
+    
+    private func setupLog() {
+        central.log = { print("Central: \($0)") }
+    }
+    
+    private func observeValues() {
+        Task { [weak self] in
+            do {
+                while let self {
+                    try await Task.sleep(for: .seconds(1))
+                    await self.updateState()
+                }
+            }
+            catch {
+                
+            }
+        }
+    }
+    
+    private func updateState() async {
+        assert(Thread.isMainThread)
+        let oldValue = self.isEnabled
+        let newValue = await self.central.isEnabled
+        guard newValue != oldValue else {
+            return
+        }
+        // update value
+        self.isEnabled = newValue
+        // start scanning when powered on
+        guard newValue else {
+            return
+        }
+        do { try await self.scan() }
+        catch { } // ignore error
+    }
+    
+    private func updateConnected() async {
+        assert(Thread.isMainThread)
+        let oldValue = self.connected
+        let newValue = await Set(central.peripherals.compactMap { $0.value ? $0.key : nil })
+        guard oldValue != newValue else {
+            return
+        }
+        self.connected = newValue
+    }
+    
+    public func log(_ message: String) {
+        print(message)
+    }
+    
+    func log(error: any Error) {
+        print("Error", error.localizedDescription)
+    }
+    
+    func scan(
+        with services: Set<BluetoothUUID> = [],
+        filterDuplicates: Bool = true
+    ) async throws {
+        scanResults.removeAll(keepingCapacity: true)
+        self.scanStream = nil // end previous scan
+        let stream = central.scan(
+            with: services,
+            filterDuplicates: filterDuplicates
+        )
+        self.scanStream = stream
+        Task {
+            for try await scanData in stream {
+                await found(scanData: scanData)
+            }
+            self.scanStream = nil
+        }
+    }
+    
+    /// Cache discovered values
+    private func found(scanData: ScanData) async {
+        var cache = scanResults[scanData.peripheral] ?? ScanDataCache(scanData: scanData)
+        cache += scanData
+        #if os(Android)
+        
+        #elseif os(iOS) && targetEnvironment(simulator)
+        
+        #elseif canImport(CoreBluetooth)
+        cache.name = try? await central.name(for: scanData.peripheral)
+        for serviceUUID in scanData.advertisementData.overflowServiceUUIDs ?? [] {
+            cache.overflowServiceUUIDs.insert(serviceUUID)
+        }
+        #endif
+        scanResults[scanData.peripheral] = cache
+    }
+    
+    func stopScan() async {
+        scanStream?.stop()
+        scanStream = nil
+    }
+    
+    func connect(to peripheral: Central.Peripheral) async throws {
+        activity[peripheral] = true
+        defer { activity[peripheral] = false }
+        if isScanning {
+            scanStream?.stop()
+        }
+        try await central.connect(to: peripheral)
+    }
+    
+    func disconnect(_ peripheral: Central.Peripheral) async {
+        await central.disconnect(peripheral)
+    }
+    
+    func discoverServices(for peripheral: Central.Peripheral) async throws {
+        activity[peripheral] = true
+        defer { activity[peripheral] = false }
+        let services = try await central.discoverServices(for: peripheral)
+        assert(Thread.isMainThread)
+        self.services[peripheral] = services
+    }
+    
+    func discoverCharacteristics(for service: Service) async throws {
+        activity[service.peripheral] = true
+        defer { activity[service.peripheral] = false }
+        let characteristics = try await central.discoverCharacteristics([], for: service)
+        assert(Thread.isMainThread)
+        self.characteristics[service] = characteristics
+    }
+    
+    func discoverIncludedServices(for service: Service) async throws {
+        activity[service.peripheral] = true
+        defer { activity[service.peripheral] = false }
+        let includedServices = try await central.discoverIncludedServices(for: service)
+        assert(Thread.isMainThread)
+        self.includedServices[service] = includedServices
+    }
+    
+    func discoverDescriptors(for characteristic: Characteristic) async throws {
+        activity[characteristic.peripheral] = true
+        defer { activity[characteristic.peripheral] = false }
+        let includedServices = try await central.discoverDescriptors(for: characteristic)
+        assert(Thread.isMainThread)
+        self.descriptors[characteristic] = includedServices
+    }
+    
+    func readValue(for characteristic: Characteristic) async throws {
+        activity[characteristic.peripheral] = true
+        defer { activity[characteristic.peripheral] = false }
+        let data = try await central.readValue(for: characteristic)
+        assert(Thread.isMainThread)
+        let value = AttributeValue(
+            date: Date(),
+            type: .read,
+            data: data
+        )
+        self.characteristicValues[characteristic, default: .init(capacity: 10)].append(value)
+    }
+    
+    func writeValue(_ data: Data, for characteristic: Characteristic, withResponse: Bool = true) async throws {
+        activity[characteristic.peripheral] = true
+        defer { activity[characteristic.peripheral] = false }
+        try await central.writeValue(data, for: characteristic, withResponse: withResponse)
+        assert(Thread.isMainThread)
+        let value = AttributeValue(
+            date: Date(),
+            type: .write,
+            data: data
+        )
+        self.characteristicValues[characteristic, default: .init(capacity: 10)].append(value)
+    }
+    
+    func notify(_ isEnabled: Bool, for characteristic: Characteristic) async throws {
+        activity[characteristic.peripheral] = true
+        defer { activity[characteristic.peripheral] = false }
+        if isEnabled {
+            let stream = try await central.notify(for: characteristic)
+            isNotifying[characteristic] = isEnabled
+            Task.detached(priority: .low) { [unowned self] in
+                for try await notification in stream {
+                    await self.notification(notification, for: characteristic)
+                }
+            }
+        } else {
+            //try await central.stopNotifications(for: characteristic)
+            isNotifying[characteristic] = false
+        }
+    }
+    
+    private func notification(_ data: Data, for characteristic: Characteristic) async {
+        assert(Thread.isMainThread)
+        let value = AttributeValue(
+            date: Date(),
+            type: .notification,
+            data: data
+        )
+        self.characteristicValues[characteristic, default: .init(capacity: 10)].append(value)
+    }
+    
+    func readValue(for descriptor: Descriptor) async throws {
+        activity[descriptor.peripheral] = true
+        defer { activity[descriptor.peripheral] = false }
+        let data = try await central.readValue(for: descriptor)
+        assert(Thread.isMainThread)
+        let value = AttributeValue(
+            date: Date(),
+            type: .read,
+            data: data
+        )
+        self.descriptorValues[descriptor, default: .init(capacity: 10)].append(value)
+    }
+    
+    func writeValue(_ data: Data, for descriptor: Descriptor) async throws {
+        activity[descriptor.peripheral] = true
+        defer { activity[descriptor.peripheral] = false }
+        try await central.writeValue(data, for: descriptor)
+        assert(Thread.isMainThread)
+        let value = AttributeValue(
+            date: Date(),
+            type: .write,
+            data: data
+        )
+        self.descriptorValues[descriptor, default: .init(capacity: 10)].append(value)
+    }
+}
+
+// MARK: - Supporting Types
+
+struct ScanDataCache <Peripheral: Peer, Advertisement: AdvertisementData>: Equatable, Hashable {
+    
+    var scanData: GATT.ScanData<Peripheral, Advertisement>
+    
+    /// GAP or advertised name
+    var name: String?
+    
+    /// Advertised name
+    var advertisedName: String?
+    
+    var manufacturerData: GATT.ManufacturerSpecificData<Advertisement.Data>?
+    
+    /// This value is available if the broadcaster (peripheral) provides its Tx power level in its advertising packet.
+    /// Using the RSSI value and the Tx power level, it is possible to calculate path loss.
+    var txPowerLevel: Double?
+    
+    /// Service-specific advertisement data.
+    var serviceData = [BluetoothUUID: Advertisement.Data]()
+    
+    /// An array of service UUIDs
+    var serviceUUIDs = Set<BluetoothUUID>()
+    
+    /// An array of one or more ``BluetoothUUID``, representing Service UUIDs.
+    var solicitedServiceUUIDs = Set<BluetoothUUID>()
+    
+    /// An array of one or more ``BluetoothUUID``, representing Service UUIDs that were found in the “overflow” area of the advertisement data.
+    var overflowServiceUUIDs = Set<BluetoothUUID>()
+    
+    /// Advertised iBeacon
+    var beacon: AppleBeacon?
+    
+    init(scanData: GATT.ScanData<Peripheral, Advertisement>) {
+        self.scanData = scanData
+        self += scanData
+    }
+    
+    static func += (cache: inout ScanDataCache, scanData: GATT.ScanData<Peripheral, Advertisement>) {
+        cache.scanData = scanData
+        cache.advertisedName = scanData.advertisementData.localName
+        if cache.name == nil {
+            cache.name = scanData.advertisementData.localName
+        }
+        cache.txPowerLevel = scanData.advertisementData.txPowerLevel
+        if let beacon = scanData.advertisementData.beacon {
+            cache.beacon = beacon
+        } else {
+            cache.manufacturerData = scanData.advertisementData.manufacturerData
+        }
+        for serviceUUID in scanData.advertisementData.serviceUUIDs ?? [] {
+            cache.serviceUUIDs.insert(serviceUUID)
+        }
+        for (serviceUUID, serviceData) in scanData.advertisementData.serviceData ?? [:] {
+            cache.serviceData[serviceUUID] = serviceData
+        }
+    }
+}
+
+extension ScanDataCache: Identifiable {
+    
+    var id: Peripheral.ID {
+        scanData.id
+    }
+}

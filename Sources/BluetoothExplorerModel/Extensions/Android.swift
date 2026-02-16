273369b3fce6e6ca00df2b261c2f42463a511699
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-02-15 21:21:46 -0500

    Add `SkipProcessInfo`

--- Sources/BluetoothExplorerModel/Extensions/Android.swift
+++ Sources/BluetoothExplorerModel/Extensions/Android.swift
@@ -7,18 +7,42 @@
 
 #if os(Android)
 import Foundation
-import JavaKit
+import SwiftJava
 import AndroidContent
 
 public extension AndroidContent.Context {
 
-    /// Get the application's Android Context from ProcessInfo.
-    static func androidContext() -> AndroidContent.Context {
-        guard let javaObject = ProcessInfo.processInfo.dynamicAndroidContext().toJavaObject(options: .kotlincompat) else {
-            fatalError("Unable to get Android context")
-        }
-        let environment = try! JavaVirtualMachine.shared().environment()
-        return AndroidContent.Context(javaThis: javaObject, environment: environment)
+    /// Get the Skip application's Android Context from ProcessInfo.
+    static func androidContext() throws -> AndroidContent.Context? {
+        try SkipProcessInfo.processInfo().getAndroidContext()
     }
 }
+
+@JavaClass("skip.foundation.ProcessInfo")
+open class SkipProcessInfo: JavaObject {
+    
+    @JavaMethod
+    @_nonoverride public convenience init(environment: JNIEnvironment? = nil)
+    
+    class func processInfo() throws -> SkipProcessInfo {
+        try JavaClass<SkipProcessInfo>().Companion.getProcessInfo()
+    }
+    
+    @JavaMethod
+    open func getAndroidContext() -> AndroidContent.Context?
+    
+    @JavaClass("skip.foundation.ProcessInfo$Companion")
+    open class Companion: JavaObject {
+        
+        @JavaMethod
+        func getProcessInfo() -> SkipProcessInfo!
+    }
+}
+
+extension JavaClass<SkipProcessInfo> {
+    
+    @JavaStaticField(isFinal: true)
+    public var Companion: SkipProcessInfo.Companion!
+}
+
 #endif
